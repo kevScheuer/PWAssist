977c902d505311b589662e37021c939c9d44dd51
kevScheuer <kscheuer@jlab.org>
2026-07-31 10:49:18 -0400

    todos for basic test parser

---
+++ tests/test_parser.py
@@ -0,0 +1,9 @@
+"""Unit test for the amplitude parser module"""
+
+from pwassist.parser import AmplitudeParser, NamingScheme, ParsedAmplitude, SchemeDef
+
+# TODO: I want to test
+# - Can the parser determine if mixed amplitude styles are used
+# - Should identify and parse sets of amplitudes in same scheme
+# - All public functions return the expected strings
+# - maybe test for jumbled alphanumeric strings
